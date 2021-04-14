5de43652741e4e80bd34190c953c3e8249fcf44b
Ian Davies <mr.daviesian@gmail.com>
2021-04-14 16:02:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+let num1 = 8
+let num2 = 8
+let num3 = 12
+let num4 = 8
+
+const multiply = (num1 * num2) - 2
+
+const random = Math.round(Math.random() * 1000)
+
+const mod = num3 % num4
+
+const max = Math.max(5, 10, 15, 20)
